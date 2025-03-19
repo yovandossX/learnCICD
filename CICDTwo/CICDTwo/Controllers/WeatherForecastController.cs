e80346711864e7448782bd209b44e548a5cdd0bb
yovandossX <yovandoss@boscosofttech.com>
2025-03-19 23:06:08 +0530

    x

--- CICDTwo/CICDTwo/Controllers/WeatherForecastController.cs
+++ CICDTwo/CICDTwo/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Chil"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Chill"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
